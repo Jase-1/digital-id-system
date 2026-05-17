50d2f10f336e27f4b8ea4fd288f4986ef5d93c99
Aniekan Jason Ejiro Roberts <a.j.e.roberts@se25.qmul.ac.uk>
2026-05-17 19:02:32 +0100

    Adding in contents within classes for each different strategy group

--- src/main/java/com/digitalid/service/DrivingLicenceVerification.java
+++ src/main/java/com/digitalid/service/DrivingLicenceVerification.java
@@ -1,4 +1,19 @@
 package com.digitalid.service;
 
-public class DrivingLicenceVerification {
+import com.digitalid.model.DigitalId;
+import com.digitalid.model.IdStatus;
+
+public class DrivingLicenceVerification implements VerificationStrategy {
+
+    @Override
+    public boolean verify(DigitalId digitalId) {
+        return digitalId.getStatus() == IdStatus.ACTIVE
+                && digitalId.getNationality() != null
+                && !digitalId.getNationality().isEmpty();
+    }
+
+    @Override
+    public String getOrganisationType() {
+        return "Driving Licence Authority";
+    }
 }
--- src/main/java/com/digitalid/service/TaxAuthorityVerification.java
+++ src/main/java/com/digitalid/service/TaxAuthorityVerification.java
@@ -1,4 +1,17 @@
 package com.digitalid.service;
 
-public class TaxAuthorityVerification {
+import com.digitalid.model.DigitalId;
+import com.digitalid.model.IdStatus;
+
+public class TaxAuthorityVerification implements VerificationStrategy {
+
+    @Override
+    public boolean verify(DigitalId digitalId) {
+        return digitalId.getStatus() == IdStatus.ACTIVE;
+    }
+
+    @Override
+    public String getOrganisationType() {
+        return "Tax Authority";
+    }
 }
